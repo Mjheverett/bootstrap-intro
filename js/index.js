daff29d5caa81a5b3545f996eb8bb167262fa7ac
Matthew Everett <matthew@mjheverett.com>
2020-08-26 14:03:03 -0400

    "Initial Commit and sandboxing bootstrap components

---
+++ js/index.js
@@ -0,0 +1,7 @@
+'use strict'
+
+$(document).ready(() => {
+    // $(function() {
+    //     $('[data-toggle="button"]').siblings(".progress").find(".progress-bar-striped").toggleClass("progress-bar-animated")
+    // });
+});
